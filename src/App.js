8b3bf44e09e10fda908341bc2f484364e12ebda3
genki0112 <110474417+genki0112@users.noreply.github.com>
2022-09-04 18:46:44 +0900

    css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { CssModules } from "./components/CssModules";
 import { InlineStyles } from "./components/InlineStyles";
 import { StyledComponents } from "./components/StyledComponents";
 import { StyledJsx } from "./components/StyleJsx";
+import { Emotion } from "./components/Emotion.jsx";
 import "./styles.css";
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
       <CssModules />
       <StyledJsx />
       <StyledComponents />
+      <Emotion />
     </div>
   );
 }
