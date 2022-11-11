93350847d091436deca4f54db2eff4e3e1258641
genki0112 <110474417+genki0112@users.noreply.github.com>
2022-11-11 17:29:14 +0900

    a a

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,7 +1,14 @@
+import { useLocation, useHistory } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const location = useLocation();
+  const history = useHistory();
+  console.log(history);
+  const onClickBack = () => history.goBack();
   return (
     <div>
       <h1>ページ１のディテールAです。</h1>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
