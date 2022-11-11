d76400349e99155d5ef22ac0f9a022a88c5734b4
genki0112 <110474417+genki0112@users.noreply.github.com>
2022-11-11 16:17:07 +0900

    a

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -5,6 +5,8 @@ export const Page2 = () => {
     <div>
       <h1>ページ２です。</h1>
       <Link to="/page2/100">URL Parameter Page</Link>
+      <br />
+      <Link to="/page2/100?name=hoge">Query Parameter Page</Link>
     </div>
   );
 };
